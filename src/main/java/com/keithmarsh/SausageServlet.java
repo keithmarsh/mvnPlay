a3b3549fd3fa13e19bdc9e53a609e2f41f7a06fe
Anonymous <keithmarsh2@gmail.com>
2015-05-17 21:51:39 +0000

    DB

--- src/main/java/com/keithmarsh/SausageServlet.java
+++ src/main/java/com/keithmarsh/SausageServlet.java
@@ -1,6 +1,7 @@
 package com.keithmarsh;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
@@ -8,12 +9,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+
 public class SausageServlet extends HttpServlet {
-  private static final long serialVersionUID = 1L;
-   Logger log = Logger.getLogger(this.getClass().getName());
-   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-     response.getWriter().println("Hello World!");
-     response.getWriter().close();
-     log.info("KJM " + request.getPathInfo()); 
-	}
+    private static final long serialVersionUID = 1L;
+    Logger log = Logger.getLogger(this.getClass().getName());
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        MongoCredential credential = MongoCredential.createCredential("kjm", "mvn-play", "unsafe".toCharArray());
+        MongoClient mongo = new MongoClient(new ServerAddress("ds031852.mongolab.com", 31852), Arrays.asList(credential));
+        DB db = mongo.getDB("mvn-play");
+        DBCollection names = db.getCollection("names");
+        DBObject object = names.findOne();
+        response.getWriter().println(object.toString());
+        response.getWriter().close();
+    }
 }
